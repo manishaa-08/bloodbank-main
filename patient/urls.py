20bf6959ef261bf39f3106b98628ba41255fdf4f
manishaa-08 <manishathakur0824@gmail.com>
2024-07-14 15:23:01 +0530

    initial commit

---
+++ patient/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from . import views
+urlpatterns = [
+    path('patientlogin', LoginView.as_view(template_name='patient/patientlogin.html'),name='patientlogin'),
+    path('patientsignup', views.patient_signup_view,name='patientsignup'),
+    path('patient-dashboard', views.patient_dashboard_view,name='patient-dashboard'),
+    path('make-request', views.make_request_view,name='make-request'),
+    path('my-request', views.my_request_view,name='my-request'),
+    path('logout/', LogoutView.as_view(template_name='blood/logout.html'),name='logout'),
+]
